1d21c0288beb5241f0da054ab362a0800a30f4fc
David Chiu <davidwchiu@gmail.com>
2018-08-17 00:57:58 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
 class CreateShows < ActiveRecord::Migration[5.1]
   def change 
-    create_table :shows do |t|
+    create_table :show do |t|
       t.string :name
       t.string :network
       t.string :day 
